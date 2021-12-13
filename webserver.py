f7e2517e8a8146bd9a0dcc85cfa35cdd64af8149
MusicalPieces <72195951+DivyamSamarwal@users.noreply.github.com>
2021-12-13 20:22:53 +0530

    Add files via upload

---
+++ webserver.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+from threading import Thread
+
+
+
+app = Flask('')
+
+
+
+@app.route('/')
+
+def home():
+
+    return "AutoBot"
+
+
+
+def run():
+
+  app.run(host='0.0.0.0',port=1000)
+
+
+
+def keep_alive():  
+
+    t = Thread(target=run)
+
+    t.start()
