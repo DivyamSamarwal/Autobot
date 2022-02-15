acecd6673c514d1d7197de1670d67856ad332101
Divyam Samarwal <divyamsamarwal@gmail.com>
2022-02-15 18:25:25 +0530

    Update anime.py

--- cogs/anime.py
+++ cogs/anime.py
@@ -3,6 +3,7 @@ import animec
 import datetime
 from datetime import datetime
 from discord.ext import commands
+from aiohttp import request
 
 class Anime(commands.Cog, name='Anime'):
     """Anime commands for weebs"""
@@ -133,6 +134,176 @@ class Anime(commands.Cog, name='Anime'):
                         voters.append(reactor.id)
         output = f"Results of the poll for **{embed.title}** :\n" + '\n'.join(['{}: {}'.format(opt_dict[key], tally[key]) for key in tally.keys()])
         await ctx.send(output)
+
+    @commands.command(name="pat", aliases=['pats', 'headpat', 'headpats'])
+ 
+    async def pat_(self, ctx: commands.Context, user: discord.Member):
+        """Pat someone"""
+        if user.bot:
+            return await ctx.send(embed=discord.Embed(description="** <a:wrong:940200609326444594> You can't pat a bot.**", color=discord.Colour.red()))
+        if user == ctx.author:
+            return await ctx.send(embed=discord.Embed(description="**<a:wrong:940200609326444594> You can't pat yourself! Try patting someone else.**", color=discord.Colour.red()))
+
+        url = "https://some-random-api.ml/animu/pat"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed= discord.Embed(description=f'{ctx.author.mention} patted {user.mention}',color=discord.Colour.green())
+                embed.set_image(url=data['link'])
+                await ctx.send(embed=embed)
+                
+            else:
+                embed = discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed)
+                #await ctx.send(f"API ERRORS {response.status}status.")
+
+
+    @commands.command(name="hug", aliases=['hugs', 'backhug', 'chesthugs'])
+   
+    async def hug_(self, ctx: commands.Context, user: discord.Member):
+        """Hug someone"""
+        if user.bot:
+            return await ctx.send(embed=discord.Embed(description="**<a:wrong:940200609326444594> You can't hug a bot.**", color=discord.Colour.red()))
+        if user == ctx.author:
+            return await ctx.send(embed=discord.Embed(description="**<a:wrong:940200609326444594> You can't hug yourself! Try hugging someone else.**", color=discord.Colour.red()))
+
+        url = "https://some-random-api.ml/animu/hug"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed = discord.Embed(description=f"{ctx.author.mention} hugs {user.mention}",color=discord.Colour.green())
+                embed.set_image(url=data['link'])
+                await ctx.send(embed=embed)
+            else:
+                embed= discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed)               
+                #await ctx.send(f"API ERRORS {response.status}status.")
+
+
+    @commands.command(name="slap")
+    async def slap_(self, ctx: commands.Context, user: discord.Member):
+        """Slap someone"""
+        if user.bot:
+            return await ctx.send(embed=discord.Embed(description="**<a:wrong:940200609326444594> You can't slap a bot.**", color=discord.Colour.red()))
+        if user == ctx.author:
+            return await ctx.send(embed=discord.Embed(description="**<a:wrong:940200609326444594> You can't slap yourself! Try slapping someone else.**", color=discord.Colour.red()))
+
+        url = "https://neko-love.xyz/api/v1/slap"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed = discord.Embed(description=f"{ctx.author.mention} slaps {user.mention}",color=discord.Colour.green())
+                embed.set_image(url=data['url'])
+                await ctx.send(embed=embed)
+            else:
+                embed= discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed)               
+                #await ctx.send(f"API ERRORS {response.status}status.")
+    
+    @commands.command(name="wink")
+
+    async def wink_(self, ctx: commands.Context):
+        """wink your eye"""
+        
+
+        url = "https://api.waifu.pics/sfw/wink"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed = discord.Embed(description=f"{ctx.author.mention} Your wink is unique",color=discord.Colour.green())
+                embed.set_image(url=data['url'])
+                await ctx.send(embed=embed)
+            else:
+                embed= discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed)               
+                #await ctx.send(f"API ERRORS {response.status}status.")
+
+    @commands.command(name="bully")
+
+    async def bully_(self, ctx: commands.Context, user: discord.Member):
+        """Bully Someone"""
+        
+
+        url = "https://api.waifu.pics/sfw/bully"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed = discord.Embed(description=f"{ctx.author.mention} bullies {user.mention}",color=discord.Colour.green())
+                embed.set_image(url=data['url'])
+                await ctx.send(embed=embed)
+            else:
+                embed= discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed) 
+
+
+    @commands.command(name="cuddle")
+    async def cuddle_(self, ctx: commands.Context, user: discord.Member):
+        """Cuddle Someone"""
+        
+
+        url = "https://api.waifu.pics/sfw/cuddle"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed = discord.Embed(description=f"{ctx.author.mention} cuddles {user.mention}",color=discord.Colour.green())
+                embed.set_image(url=data['url'])
+                await ctx.send(embed=embed)
+            else:
+                embed= discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed)
+
+
+
+    @commands.command(name="cry")
+    async def cry_(self, ctx: commands.Context, * , reason=None):
+        """Anime Cry for something"""
+        
+
+        url = "https://api.waifu.pics/sfw/cry"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed = discord.Embed(description=f"{ctx.author.mention} is crying for \n reason : **{reason}**",color=discord.Colour.green())
+                embed.set_image(url=data['url'])
+                await ctx.send(embed=embed)
+            else:
+                embed= discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed)
+
+
+    @commands.command(name="kiss")
+
+    async def kiss_(self, ctx: commands.Context, user: discord.Member):
+        """Kiss someone"""
+        
+
+        url = "https://api.waifu.pics/sfw/kiss"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed = discord.Embed(description=f"{ctx.author.mention} kisses {user.mention}",color=discord.Colour.green())
+                embed.set_image(url=data['url'])
+                await ctx.send(embed=embed)
+            else:
+                embed= discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed)    
+
+    @commands.command(name="waifu")
+    async def waifu(self, ctx: commands.Context, * , reason=None):
+        """Anime Cry for something"""
+        
+
+        url = "https://api.waifu.pics/sfw/waifu"
+        async with request("GET", url, headers={}) as response:
+            if response.status == 200:
+                data = await  response.json()
+                embed = discord.Embed(description=f"<:keqing:939052537699512340>",color=discord.Colour.random())
+                embed.set_image(url=data['url'])
+                await ctx.send(embed=embed)
+            else:
+                embed= discord.Embed(description=f"API ERRORS {response.status}status.",color=discord.Colour.red())
+                await ctx.send(embed=embed)
+
 
 
 def setup(client):
